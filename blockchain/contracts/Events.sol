ff983519a9bd5399361f337d61bf3e90ad172a8e
WSR <Alexander Voronkov>
2018-10-12 10:18:24 +0300

    причесал, проверяй

--- blockchain/contracts/Events.sol
+++ blockchain/contracts/Events.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.4.24;
 // Контракт, содержащий все события,
 // имитируемые смарт-контрактом
 contract Events {
+    // Событие создания заявки
     event ApplicationCreated (address _creator, uint256 _appId);
+    // Событие изменения заявки
     event ApplicationEdited (address _creator, uint256 _appId);
+    // Событие принятия заявки
     event ApplicationAccepted (address _moderator, uint256 _appId);
+    // Событие отправки сообщения
     event MessageSent (uint256 _appId);
+    // Событие закрытия заявки
     event ApplicationClosed (uint256 _appId);
 }
--- blockchain/test/test-moderators.js
+++ blockchain/test/test-moderators.js
@@ -2,9 +2,13 @@ var TechSupport = artifacts.require('TechSupport');
 
 contract('Moderator', function(accounts) {
 
+    // Переменная, хранящая контракт
     var contract;
+    // Переменная, хранящая аккаунт создателя контракта
     var owner = accounts[0];
 
+    // Создание нового instance контракта
+    // перед вызовом каждого юнит-теста
     beforeEach(function() {
         return TechSupport.new({from: owner})
         .then(function(instance) {
@@ -12,12 +16,15 @@ contract('Moderator', function(accounts) {
         });
     });
 
+    // Контракт должен инициализироваться с нужным
+    // аккаунтом администратора
     it('should be able to correctly set admin', function() {
         return contract.admin().then(function(result) {
             assert.equal(result, accounts[0]);
         });
     });
 
+    // Главный администратор должен уметь добавлять модераторов
     it('admin should be able to set moderators', function() {
         return contract.changeModerator(1, accounts[1], {from: accounts[0]}).then(function() {
             return contract.moderators.call(accounts[1], {from: accounts[0]}).then(function(result) {
